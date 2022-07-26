3bfd245a3b67e1474f667435a3415a64410769fd
piotr-kostka <102327925+piotr-kostka@users.noreply.github.com>
2022-07-26 17:56:03 +0200

    KOD-19.3 Zadanie: usuwanie zadania

--- src/main/java/com/crud/tasks/service/DbService.java
+++ src/main/java/com/crud/tasks/service/DbService.java
@@ -27,4 +27,8 @@ public class DbService {
     public Task saveTask(final Task task) {
         return repository.save(task);
     }
+
+    public void deleteTask(final Long taskId) {
+        repository.deleteById(taskId);
+    }
 }
