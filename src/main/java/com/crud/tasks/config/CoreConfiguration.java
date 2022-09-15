f7d6d37385680d4682ed9deaf7cad3f47cd9d04b
piotr-kostka <102327925+piotr-kostka@users.noreply.github.com>
2022-09-15 23:35:48 +0200

    23.4. Implementacja maila w projekcie oraz adnotacja @Scheduled

--- src/main/java/com/crud/tasks/config/CoreConfiguration.java
+++ src/main/java/com/crud/tasks/config/CoreConfiguration.java
@@ -3,6 +3,7 @@ package com.crud.tasks.config;
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.web.client.RestTemplate;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
@@ -10,6 +11,7 @@ import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
+@EnableScheduling
 @OpenAPIDefinition //zamiast @EnableSwagger2
 @Configuration
 public class CoreConfiguration {
